20ccbc83f7490344a745e53fcaf8fd777201d089
Lukáš Braburda <breburda.dolakova@gmail.com>
2018-02-19 15:00:59 +0100

    add_page

---
+++ scoreboard_dlouhodobka/scoreboard_dlouhodobka/scoreboard_dlouhodobka/pages/about.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace scoreboard_dlouhodobka.pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class about : ContentPage
+    {
+        public about()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ scoreboard_dlouhodobka/scoreboard_dlouhodobka/scoreboard_dlouhodobka/pages/sport_main.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace scoreboard_dlouhodobka.pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class sport_main : ContentPage
+    {
+        public sport_main()
+        {
+            InitializeComponent();
+        }
+    }
+}
